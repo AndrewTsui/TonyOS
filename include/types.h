00582dfb795721c8eaa95bde551eab6136deb8cc
AndrewTsui <xzq1258111610@hotmail.com>
2021-08-21 02:30:06 -0700

    commit include/

---
+++ include/types.h
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  types.h
+ *
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *       Author:  Hurley (LiuHuan), liuhuan1992@gmail.com
+ *       Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+#ifndef INCLUDE_TYPES_H_
+#define INCLUDE_TYPES_H_
+
+#ifndef NULL
+	#define NULL 0
+#endif
+
+#ifndef TRUE
+	#define TRUE  1
+	#define FALSE 0
+#endif
+
+typedef unsigned int   uint32_t;
+typedef          int   int32_t;
+typedef unsigned short uint16_t;
+typedef          short int16_t;
+typedef unsigned char  uint8_t;
+typedef          char  int8_t;
+
+#endif 	// INCLUDE_TYPES_H_
